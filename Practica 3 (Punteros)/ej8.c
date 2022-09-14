1d1cfcaff982e0e442b395360fb6d182f03cf89b
rokor12 <35020329+rokor12@users.noreply.github.com>
2022-09-14 17:49:42 -0300

    Cargar String Dinámico 2

---
+++ Practica 3 (Punteros)/ej8.c
@@ -0,0 +1,60 @@
+/*
+Desarrollar dos versiones de la función del ejercicio anterior cargarStrDin() según el siguiente detalles:
+Versiones de cargarStrDin():
+	1.	Esta versión char* cargarStrDin1(), no se le pasan parámetros y retorna la
+		dirección de memoria del arreglo dinámico creado.
+	2.	Esta versión void cargarStrDin2(char**), no retorna ningún valor, y se le
+		pasa por parámetro la dirección de memoria de un puntero a char.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+//#include <math.h>
+//#include <time.h>
+
+char* cargarStrDin1(); //ej7
+void cargarStrDin2(char* str);
+
+int main(){
+	char *str1 = cargarStrDin1();
+	printf("%s\n", str1);
+	char *str2 = (char*)malloc(sizeof(char)); //requiere estar inicializado
+	cargarStrDin2(str2);
+	printf("%s\n", str2);
+	return 0;
+}
+
+void cargarStrDin2(char* str){
+	int size = 0;
+	char c;
+	printf("cargar String2: ");
+	c = getchar();
+	while(c!='\n'){
+		size++;
+		str = realloc(str,size*sizeof(char));
+		str[size-1] = c;
+		c = getchar();
+	}
+	size++;
+	str = realloc(str,size*sizeof(char));
+	str[size-1] = 0;
+}
+
+char* cargarStrDin1(){
+	char *str = (char*)malloc(sizeof(char));
+	int size = 0;
+	char c;
+	printf("cargar String1: ");
+	c = getchar();
+	while(c!='\n'){
+		size++;
+		str = realloc(str,size*sizeof(char));
+		str[size-1] = c;
+		c = getchar();
+	}
+	size++;
+	str = realloc(str,size*sizeof(char));
+	str[size-1] = 0;
+	return str;
+}
