472e030eb4fa4c9fe57bd6c1e57970fa2a2721b1
rokor12 <35020329+rokor12@users.noreply.github.com>
2022-09-16 02:37:10 -0300

    -

--- Practica 3 (Punteros)/ej12.c
+++ Practica 3 (Punteros)/ej12.c
@@ -11,38 +11,50 @@ int longitud;
 };
 typedef struct s_texto t_texto;
 
-void cargarTexto(t_texto*);
-t_texto** cargarTextos();
+void cargarTexto(t_texto*);				// 3.10
+t_texto** cargarTextos();				// 3.12.a
+void ordenar(t_texto**);				// 3.12.b
+void intercambio(t_texto*, t_texto*);	// 3.12.b
+void comparar(t_texto*, t_texto*);
 void imprimir(t_texto**);
 
 int main(){
 	t_texto **strings = cargarTextos();
-	cargarTextos(strings);
 	imprimir(strings);
+	ordenar(strings);
+	imprimir(strings);
+	return 0;
+}
+
+void ordenar(t_texto** strings){
+	int i,j;
+	for(i=0;strcmp(strings[i]->txt,"");i++){
+		for(j=i+1;strcmp(strings[j]->txt,"");j++){
+			if(strcmp(strings[i]->txt,strings[j]->txt)>0){
+				intercambio(strings[i],strings[j]);
+		    }
+		}
+	}
 }
 
 void imprimir(t_texto** strings){
 	int i=0;
-	printf("TEST:\n");
+	printf("\nFrases:\n");
 	while(strings[i]->longitud!=0){
-		printf("%s\n",strings[i]->txt);
+		printf("%d, %s\n",strings[i]->longitud,strings[i]->txt);
 		i++;
 	}
 }
 
 t_texto** cargarTextos(){
-	t_texto** strings = (t_texto**)malloc(sizeof(t_texto*));
-	int size = 0;
-	size++;
-	strings = realloc(strings,size*sizeof(t_texto*));
-	strings[size-1] = (t_texto*)malloc(sizeof(t_texto));
-	cargarTexto(strings[size-1]);
-	while(strlen(strings[size-1]->txt)!=0){
-		size++;
+	t_texto **strings = (t_texto**)malloc(sizeof(t_texto*));
+	int size = 1,aux=1;
+	while(aux!=0){
 		strings = realloc(strings,size*sizeof(t_texto*));
 		strings[size-1] = (t_texto*)malloc(sizeof(t_texto));
 		cargarTexto(strings[size-1]);
-		printf("%d\n",strings[size-1]->longitud);
+		aux = strings[size-1]->longitud;
+		size++;
 	}
 	return strings;
 }
@@ -51,7 +63,7 @@ void cargarTexto(t_texto* str){
 	str->txt = (char*)malloc(sizeof(char));
 	int size = 0;
 	char c;
-	printf("cargar String: ");
+	printf("Cargar Frase: ");
 	c = getchar();
 	while(c!='\n'){
 		size++;
@@ -63,4 +75,10 @@ void cargarTexto(t_texto* str){
 	str->txt = realloc(str->txt,size*sizeof(char));
 	str->txt[size-1] = '\0';
 	str->longitud = size-1;
+}
+
+void intercambio(t_texto *a, t_texto *b){
+	t_texto aux = *a;
+	*a = *b;
+	*b = aux;
 }
