4196fe81cc5b071ebbea8d4175997aa6b7ffcb68
rokor12 <35020329+rokor12@users.noreply.github.com>
2022-10-24 14:56:55 -0300

    Resto De La División

---
+++ Practica 4 (Recursividad)/ej03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cociente(int a, int b);
+
+int main(){
+	int a;
+	int b;
+	printf("Ingrese el primer valor: ");
+	scanf("%d",&a);
+	printf("Ingrese el segundo valor: ");
+	scanf("%d",&b);
+	printf("Resultado: %d\n",cociente(a,b));
+	return 0;
+}
+
+int cociente(int a, int b){
+	if(a>=b){
+		return cociente(a-b,b);
+	}
+	return a;
+}
